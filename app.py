98466d65d1a1b69f6caa5fecc73b7d3652183291
saktil <leosaragih001@gmail.com>
2024-08-25 17:07:33 +0700

    update the app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
+import socket
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def hello():
+    html = """Hello {name}!
+    Hostname: {hostname}"""
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
